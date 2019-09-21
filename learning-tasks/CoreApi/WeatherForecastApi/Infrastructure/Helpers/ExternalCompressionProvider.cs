31501bec1fa3d04f7a71d91b2eb1b577c5c2dcca
amshekar <amshekar@hotmail.com>
2019-09-21 19:24:56 +0530

    added custom provider

--- learning-tasks/CoreApi/WeatherForecastApi/Infrastructure/Helpers/ExternalCompressionProvider.cs
+++ learning-tasks/CoreApi/WeatherForecastApi/Infrastructure/Helpers/ExternalCompressionProvider.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.ResponseCompression;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WeatherForecastApi.Infrastructure.Helpers
 {
-    public class ExternalCompressionProvider
+    public class ExternalCompressionProvider: ICompressionProvider
     {
+        public string EncodingName => "externalcustomcompression";
+        public bool SupportsFlush => true;
+
+        public Stream CreateStream(Stream outputStream)
+        {
+            // Create a custom compression stream wrapper here
+            return outputStream;
+        }
     }
 }
